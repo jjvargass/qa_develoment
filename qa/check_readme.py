6e8cac6b45cdc5b0463d8109fe8be8efa5b56ad3
jjvargass <jotavargas89@gmail.com>
2020-07-07 06:17:39 -0500

    fix:ajustes check_readme se optimiza la validación

--- qa/check_readme.py
+++ qa/check_readme.py
@@ -8,12 +8,23 @@ logging.basicConfig(level=logging.CRITICAL)
 _logger = logging.getLogger('CHECK_README')
 _logger.setLevel(logging.DEBUG)
 
+def check_words_in_line(line, words, contador, words_encontradas):
+    new_words = words[:]
+    for i in words:
+        if i in line:
+            _logger.info(i)
+            contador +=1
+            words_encontradas.append(i)
+            new_words.remove(i)
+    return contador, words_encontradas, new_words
+
 def check_readme():
     words = ['Especificaciones Técnicas',
-                'Requerimientos de Software',
-                'Tecnologías Implementadas y Versiones',
-                'Ejecución del Proyecto',
-            'Licencia'
+             'Tecnologías Implementadas y Versiones',
+             'Variables de Entorno',
+             'Ejecución del Proyecto',
+             'Estado CI',
+             'Licencia'
             ]
     control = 0.0
     words_encontradas = []
@@ -30,29 +41,10 @@ def check_readme():
             _logger.info("DEFINICIONES ENCONTRADAS:")
             _logger.info("====")
         for line in all_line:
-            if  words[0] in line:
-                _logger.info(words[0])
-                total_encontradas +=1
-                words_encontradas.append(words[0])
-            if  words[1] in line:
-                _logger.info(words[1])
-                total_encontradas +=1
-                words_encontradas.append(words[1])
-            if  words[2] in line:
-                _logger.info(words[2])
-                total_encontradas +=1
-                words_encontradas.append(words[2])
-            if  words[3] in line:
-                _logger.info(words[3])
-                total_encontradas +=1
-                words_encontradas.append(words[3])
-            if  words[4] in line:
-                _logger.info(words[4])
-                total_encontradas +=1
-                words_encontradas.append(words[4])
+            total_encontradas, words_encontradas, words  = check_words_in_line(line, words, total_encontradas, words_encontradas)
 
         # Validar Definiciones
-        if (len(words_encontradas) == len(words)):
+        if (len(words) == 0):
             _logger.info("=================================")
             _logger.info("Se Encontraron Todas las Definiciones en el README")
         elif (len(words_encontradas) == 0):
@@ -62,8 +54,7 @@ def check_readme():
             _logger.info("=================================")
             _logger.info("DEFINICIONES NO ENCONTRADAS:")
             _logger.info("====")
-            words_no_encontradas =  list(set(words).difference(words_encontradas))
-            for pp in words_no_encontradas:
+            for pp in words:
                 _logger.info(pp)
 
         # Resultado del Check
@@ -73,14 +64,13 @@ def check_readme():
         if (total_encontradas > control):
             _logger.info("Cumple con los lineamientos")
             _logger.info("=================================")
-
         else:
             _logger.error("No Cumple con los Lineamientos")
             raise Exception('No Cumple con los Lineamientos')
 
     except IOError:
-            _logger.error("=================================")
-            _logger.error("No Existe Arvhivo README.md - No Cumple con los Lineamientos")
+            _logger.info("No Existe Arvhivo README.md - No Cumple con los Lineamientos")
+            _logger.info("=================================")
             raise Exception('No Existe Arvhivo README.md - No Cumple con los Lineamientos')
 
 if __name__ == '__main__':
